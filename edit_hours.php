625aee8f284aeee636ef9f97fb972743ee79db4c
Roman Alexy <alexy.roman@alumni.enac.fr>
2023-09-28 19:57:43 +0200

    change hours

--- edit_hours.php
+++ edit_hours.php
@@ -32,8 +32,8 @@
                                 <?php
                                     $days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
                                     $days_id = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-                                    $options_opening = ['18:00', '19:00'];
-                                    $options_closing = ['21:00', '22:00'];
+                                    $options_opening = ['18:00', '18:15', '18:30', '18:45', '19:00', '19:15', '19:30', '18:45', '20:00'];
+                                    $options_closing = ['20:30', '20:45', '21:00', '21:15', '21:30', '21:45', '22:00', '22:15', '22:30', '22:45', '23:00'];
                                     
                                     for ($i = 0; $i < count($days); $i++) {
                                         $day = $days[$i];
