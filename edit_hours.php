90f9b54aea6fb3237313ff37f2594664e742371c
Roman Alexy <alexy.roman@alumni.enac.fr>
2023-09-28 17:23:07 +0200

    disable select when closed

--- edit_hours.php
+++ edit_hours.php
@@ -7,18 +7,31 @@ document.addEventListener('DOMContentLoaded', function () {
     // Add an event listener to each checkbox and its associated select element
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
     checkboxes.forEach(function (checkbox) {
-        const associatedSelect = document.querySelector('select[name="' + checkbox.id.replace('closed', '') + '"]');
+        const associatedOpeningSelect = document.querySelector('select[name="' + checkbox.id.replace('closed', '') + '"]');
+        const associatedClosingSelect = document.querySelector('select[name="' + checkbox.id.replace('closed', '').replace('Opening', 'Closing') + '"]');
         
         // Add an event listener to the checkbox
         checkbox.addEventListener('change', function () {
             if (checkbox.checked) {
-                associatedSelect.disabled = true;
+                associatedOpeningSelect.disabled = true;
+                associatedClosingSelect.disabled = true;
             } else {
-                associatedSelect.disabled = false;
+                associatedOpeningSelect.disabled = false;
+                associatedClosingSelect.disabled = true;
             }
         });
     });
 });
+
+// Get all elements whose IDs match the pattern "closedsundayOpening"
+const elementsToChange = document.querySelectorAll('[id^="closedsundayOpening"]');
+
+// Loop through the matched elements and change their IDs
+elementsToChange.forEach(element => {
+  const newId = element.id.replace('closed', 'Closing').replace('Opening', ''); // Replace 'closed' with 'Closing' and 'Opening' with an empty string
+  element.id = newId; // Set the new ID
+});
+
 
 </script>
 </head>
