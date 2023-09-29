c98dd79434846f7d49dd89df3f24173ef945430f
Roman Alexy <alexy.roman@alumni.enac.fr>
2023-09-29 13:25:00 +0200

    put opening_hours code in a php file in order to include it

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html data-bs-theme="light" lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Nous contacter</title>
+</head>
+
+<body>
+    
+<?php
+include 'opening_hours.php';
+?>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="scripts/displayHours.js"></script>
+
+</body>
+
+</html>
---
+++ opening_hours.php
@@ -0,0 +1,18 @@
+<section class="d-flex align-items-center align-items-md-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <section>
+                    <h2 class="text-center" style="font-size: 40px; text-align: center">Nos horaires:</h2>
+                    <div id="mondayHours" style="text-align: center;"></div>
+                    <div id="tuesdayHours" style="text-align: center;"></div>
+                    <div id="wednesdayHours" style="text-align: center;"></div>
+                    <div id="thursdayHours" style="text-align: center;"></div>
+                    <div id="fridayHours" style="text-align: center;"></div>
+                    <div id="saturdayHours" style="text-align: center;"></div>
+                    <div id="sundayHours" style="text-align: center;"></div>
+                </section>
+            </div>
+        </div>
+    </div>
+</section>
